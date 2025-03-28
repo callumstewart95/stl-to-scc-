d73c164adb0ef359ae3d715e0837994c5a3850d6
callumstewart95 <60073817+callumstewart95@users.noreply.github.com>
2025-03-28 17:13:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def sanitize_text(text):
     text = re.sub(r'[\x00-\x1F\x7F]', '', text)
 
    # Remove specific unwanted characters like , , ,  (add more if needed)
-    unwanted_chars = ['\x16', '\x01', '\x0e', '\x11']  # Specific unwanted characters
+    unwanted_chars = ['\x16', '\x01', '\x0e', '\x11', '\x00', 'ÿ', '', '\x0C']
     for char in unwanted_chars:
         text = text.replace(char, " ")
 
